8615566905df7e8bb8463f3f5a64f74c10162cb4
Колесников Влад <vkolesnikov@softrust.ru>
2023-01-09 11:14:38 +0300

    eslint fix 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,30 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "standard"],
+  extends: "plugin:react/recommended",
+  overrides: [],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
+    ecmaVersion: "latest",
     sourceType: "module",
   },
   plugins: ["react"],
   rules: {
-    semi: [2, "always"],
-    indent: [0, 4],
-    "space-before-function-paren": [
-      "error",
-      { anonymous: "always", named: "never" },
-    ],
-    "multiline-ternary": ["off"],
-    quotes: [
-      "error",
-      "double",
-      {
-        allowTemplateLiterals: true,
-        awoidEscape: true,
-      },
-    ],
+    "react-hooks/exhaustive-deps": 0,
   },
 };
